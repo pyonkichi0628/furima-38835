a1abb53c1876c9e7f4d4c5187b7ec8031636d6fe
pyonkichi0628 <pyonkichi0628@gmail.com>
2023-01-16 19:32:38 +0900

    ユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,4 +10,10 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  # 新規登録時、emailとencrypted_password以外もストロングパラメーターとして許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+  end
+
 end
