5bf2cd99633263e14f95d58e8be36df803d6468b
pyonkichi0628 <pyonkichi0628@gmail.com>
2023-01-22 14:55:00 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  #下記ページではログインが必要
+  before_action :authenticate_user!, only: [:new, :create, :edit]
  
 
   def index
@@ -23,6 +24,22 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    if current_user.id != @product.user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.valid?
+      redirect_to product_path(product_params)
+    else
+      render 'edit'
+    end
+  end
 
   def product_params
     params.require(:product).permit(:image, :name, :explanation, :category_id, :condition_id, :shipping_cost_id,
