94f2c8eefd22e35f38255d2a9e0dcde521b00cbf
pyonkichi0628 <pyonkichi0628@gmail.com>
2023-02-02 20:20:21 +0900

    自動デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,9 @@ set :application, 'furima-38835'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:pyonkichi0628/furima-38835.git'
 
+#ブランチを指定する
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
